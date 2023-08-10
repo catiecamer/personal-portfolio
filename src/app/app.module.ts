74455d47fe2fe8ec3340be0f609279cdf115de55
Catie Camer <catiecamer@Caties-MacBook-Air.local>
2023-08-10 19:22:10 -0400

    build made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +12,7 @@ import { AboutComponent } from './components/about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { EmailComponent } from './components/email/email.component';
 import { ResumeComponent } from './components/resume/resume.component';
-
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -23,30 +22,19 @@ import { ResumeComponent } from './components/resume/resume.component';
     NavbarComponent,
     EmailComponent,
     ResumeComponent,
- 
-    
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
     LayoutModule,
-    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-colorChange1: boolean = true;
-
-  // scroll = function(window.scrollY){
-  //   if (scrollY>700){
-  //     colorChange1 == true;
-  //   }
-  //   else{
-  //       (".black").css("background" , "#333");  	
-  //   }
-  // }
+export class AppModule {
+  colorChange1: boolean = true;
 }
